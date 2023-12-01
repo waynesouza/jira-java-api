2bedda1e35de4cf3c049b9ceb544449dcf60de2f
Wayne <waynesouza07@gmail.com>
2023-12-01 09:24:13 -0300

    feat: added try/catch

--- src/main/java/com/example/demo/service/JiraService.java
+++ src/main/java/com/example/demo/service/JiraService.java
@@ -1,13 +1,16 @@
 package com.example.demo.service;
 
 import com.atlassian.jira.rest.client.api.JiraRestClient;
+import com.atlassian.jira.rest.client.api.RestClientException;
 import com.atlassian.jira.rest.client.api.domain.Issue;
 import com.atlassian.jira.rest.client.api.domain.input.TransitionInput;
 import com.example.demo.dto.IssueResponseDTO;
 import com.example.demo.dto.UpdateIssueRequestDTO;
 import com.example.demo.mapper.CustomConverter;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,14 +29,26 @@ public class JiraService implements CustomConverter {
     }
 
     public List<IssueResponseDTO> getAllIssues(String query) {
-        List<Issue> issues = StreamSupport.stream(jiraRestClient.getSearchClient()
-                .searchJql(query).claim().getIssues().spliterator(), false).toList();
-        return issues.stream().map(i -> mapper.map(i, IssueResponseDTO.class)).collect(Collectors.toList());
+        try {
+            List<Issue> issues = StreamSupport.stream(jiraRestClient.getSearchClient()
+                    .searchJql(query).claim().getIssues().spliterator(), false).toList();
+            return convertToList(issues);
+        } catch (RestClientException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
     }
 
     public void updateIssue(UpdateIssueRequestDTO requestDto) {
-        jiraRestClient.getIssueClient()
-                .transition(jiraRestClient.getIssueClient().getIssue(requestDto.getIdOrKey()).claim(),
-                        new TransitionInput(requestDto.getIdTransition()));
+        try {
+            jiraRestClient.getIssueClient()
+                    .transition(jiraRestClient.getIssueClient().getIssue(requestDto.getIdOrKey()).claim(),
+                            new TransitionInput(requestDto.getIdTransition()));
+        } catch (RestClientException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private List<IssueResponseDTO> convertToList(List<Issue> issues) {
+        return issues.stream().map(i -> mapper.map(i, IssueResponseDTO.class)).collect(Collectors.toList());
     }
 }
